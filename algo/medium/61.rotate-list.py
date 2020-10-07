7f3d5bd6c0ca2915590d8db76526f3fb568e82c2
jiongnanliu <jia7833@hotmail.com>
2020-10-07 13:15:18 -0400

    new solution

---
+++ algo/medium/61.rotate-list.py
@@ -0,0 +1,58 @@
+"""
+Given a linked list, rotate the list to the right by k places, where k is non-negative.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+Example 2:
+
+Input: 0->1->2->NULL, k = 4
+Output: 2->0->1->NULL
+Explanation:
+rotate 1 steps to the right: 2->0->1->NULL
+rotate 2 steps to the right: 1->2->0->NULL
+rotate 3 steps to the right: 0->1->2->NULL
+rotate 4 steps to the right: 2->0->1->NULL
+
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if not head:
+            return None
+        if not head.next:
+            return head
+        
+        length = 1
+        tail = head
+        
+        while tail.next:
+            tail = tail.next
+            length += 1
+            
+        k = k % length
+        if not k:
+            return head
+        
+        tail.next = head
+        new_tail = None
+        new_head = head
+        l = 1
+        while new_head.next and l != length - k + 1:
+            new_tail = new_head
+            new_head = new_head.next
+            l += 1
+        
+        new_tail.next = None
+        head = new_head
+        return head
+
+
